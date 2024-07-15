6c9ee34a3ca03567bce2e3c6791c7cd9bed0c4df
Kevin Reyes <kerpa9@gmail.com>
2024-07-15 12:12:56 -0500

    version ls_pro terminada

--- ls_pro.js
+++ ls_pro.js
@@ -1,18 +1,42 @@
 const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const folder = process.argv[2] ?? ".";
 
-fs.readdir(folder)
-  .then((files) => {
-    files.forEach((file) => {
-      const filePath = path.join(folder, file);
-      fs.stat(filePath);
-      console.log(file);
-    });
-  })
-  .catch((err) => {
-    if (err) {
-      console.error("Error", err);
-      return;
+async function ls(folder) {
+  let files;
+  try {
+    files = await fs.readdir(folder);
+  } catch {
+    console.error(`No se ha podido leer el directorio ${folder}`);
+    process.exit(1);
+  }
+
+  const filesPromises = files.map(async (file) => {
+    const filePath = path.join(folder, file);
+
+    let stats;
+    try {
+      stats = await fs.stat(filePath);
+    } catch {
+      console.error(`No se ha podido leer el directorio ${filePath}`);
+      process.exit(1);
     }
+
+    const isDirectrory = stats.isDirectory(),
+      fileType = isDirectrory ? "d" : "-",
+      fileSize = stats.size,
+      fileModified = stats.mtime.toLocaleString();
+
+    return `${fileType} ${file.padEnd(20)} ${fileSize
+      .toString()
+      .padStart(10)} ${fileModified}`;
   });
+
+  const filesInfo = await Promise.all(filesPromises);
+
+  filesInfo.forEach((fileInfo) => {
+    console.log(fileInfo);
+  });
+}
+ls(folder);
