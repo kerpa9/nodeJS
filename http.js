8825175c477d1e552b0615fc33186c1c6f939e8d
Kevin Reyes <kerpa9@gmail.com>
2024-08-08 14:42:28 -0500

    update version

--- http.js
+++ http.js
@@ -1,14 +1,21 @@
 const http = require("node:http");
-const { findAvailablePort } = require("./puerto-libre");
 
 const desiredPort = process.env.PORT ?? 3000;
-const server = http.createServer((req, res) => {
-  console.log("request recibida");
-  res.end("hola mundo");
-});
+const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("  <h1><b>Bienvenido a mi página de inicio</b></h1>");
+  } else if (req.url === "/contacto") {
+    res.statusCode = 200;
+    res.end("<h2>Contacto</h2>");
+  } else {
+    res.statusCode = 404;
+    res.end("<h2>404</h2>");
+  }
+};
+const server = http.createServer(processRequest);
 
-findAvailablePort(desiredPort).then((port) => {
-  server.listen(port, () => {
-    console.log(`server listening on port http://localhost:${port}`);
-  });
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`);
 });
