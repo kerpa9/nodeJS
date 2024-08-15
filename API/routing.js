290436e9dd1c97793b2276e47dcc9f9113df42c4
Kevin Reyes <kerpa9@gmail.com>
2024-08-15 16:54:59 -0500

    version post and get

--- API/routing.js
+++ API/routing.js
@@ -1,5 +1,6 @@
 const http = require("node:http");
 
+// commonJS -> modulos clásicos de node
 const dittoJSON = require("./pokemon/ditto.json");
 
 const processRequest = (req, res) => {
@@ -11,32 +12,46 @@ const processRequest = (req, res) => {
         case "/pokemon/ditto":
           res.setHeader("Content-Type", "application/json; charset=utf-8");
           return res.end(JSON.stringify(dittoJSON));
-
         default:
-          res.statusCode = 400;
+          res.statusCode = 404;
           res.setHeader("Content-Type", "text/html; charset=utf-8");
           return res.end("<h1>404</h1>");
       }
 
     case "POST":
       switch (url) {
-        case "/pokemon":
+        case "/pokemon": {
           let body = "";
 
-          //   Escuchar el evento data, es decir la información que se esta pasando por el cuerpo
-
+          // escuchar el evento data
           req.on("data", (chunk) => {
             body += chunk.toString();
           });
-      }
 
-    default:
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain; charset=utf-8");
-      return res.end("404 Not found");
+          req.on("end", () => {
+            const data = JSON.parse(body);
+            // llamar a una base de datos para guardar la info
+            res.writeHead(201, {
+              "Content-Type": "application/json; charset=utf-8",
+            });
+
+            data.timestamp = Date.now();
+            res.end(JSON.stringify(data));
+          });
+
+          break;
+        }
+
+        default:
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/plain; charset=utf-8");
+          return res.end("404 Not Found");
+      }
   }
 };
 
 const server = http.createServer(processRequest);
 
-server.listen(3100, () => {});
+server.listen(3100, () => {
+  console.log("server listening on port http://localhost:3100");
+});
